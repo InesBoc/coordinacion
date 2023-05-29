35faa11a0158b13fb48206e4e5af6edce81c1782
InesBoc <120464825+InesBoc@users.noreply.github.com>
2023-05-28 21:46:52 -0300

    Add files via upload

---
+++ JS/horarios.js
@@ -0,0 +1,25 @@
+const mostrarHorarios1= document.getElementById("mostrarHorarios1");
+const abrir1= document.getElementById("abrir1");
+const cerrar1= document.getElementById("cerrar1");
+
+abrir1.addEventListener("click",()=> {
+    mostrarHorarios1.showModal();
+  
+});
+cerrar1.addEventListener("click",()=> {
+    mostrarHorarios1.close();
+    
+
+});
+const mostrarHorarios2= document.getElementById("mostrarHorarios2");
+const abri2r= document.getElementById("abrir2");
+const cerrar2= document.getElementById("cerrar2");
+
+abrir2.addEventListener("click",()=> {
+    mostrarHorarios2.showModal();
+  
+});
+cerrar2.addEventListener("click",()=> {
+    mostrarHorarios2.close();
+
+});
---
+++ JS/staff.js
@@ -0,0 +1,73 @@
+const mostrarInfantiles= document.getElementById("infantilesDT");
+const abrir1= document.getElementById("abrir1");
+const cerrar1= document.getElementById("cerrar1");
+
+abrir1.addEventListener("click",()=> {
+    mostrarInfantiles.showModal();
+  
+});
+cerrar1.addEventListener("click",()=> {
+    mostrarInfantiles.close();
+    
+
+});
+const mostrarJuveniles= document.getElementById("juvenilesDT");
+const abrir2= document.getElementById("abrir2");
+const cerrar2= document.getElementById("cerrar2");
+
+abrir2.addEventListener("click",()=> {
+    mostrarJuveniles.showModal();
+  
+});
+cerrar2.addEventListener("click",()=> {
+    mostrarJuveniles.close();
+
+});
+const mostrarSuperiores= document.getElementById("superioresDT");
+const abrir3= document.getElementById("abrir3");
+const cerrar3= document.getElementById("cerrar3");
+
+abrir3.addEventListener("click",()=> {
+    mostrarSuperiores.showModal();
+  
+});
+cerrar3.addEventListener("click",()=> {
+    mostrarSuperiores.close();
+
+});
+const mostrarSegundas= document.getElementById("segundasDT");
+const abrir4= document.getElementById("abrir4");
+const cerrar4= document.getElementById("cerrar4");
+
+abrir4.addEventListener("click",()=> {
+    mostrarSegundas.showModal();
+  
+});
+cerrar4.addEventListener("click",()=> {
+    mostrarSegundas.close();
+
+});
+const mostrarArqueras= document.getElementById("arquerasDT");
+const abrir5= document.getElementById("abrir5");
+const cerrar5= document.getElementById("cerrar5");
+
+abrir5.addEventListener("click",()=> {
+    mostrarArqueras.showModal();
+  
+});
+cerrar5.addEventListener("click",()=> {
+    mostrarArqueras.close();
+
+});
+const mostrarPrepF= document.getElementById("pfs");
+const abrir6= document.getElementById("abrir6");
+const cerrar6= document.getElementById("cerrar6");
+
+abrir6.addEventListener("click",()=> {
+    mostrarPrepF.showModal();
+  
+});
+cerrar6.addEventListener("click",()=> {
+    mostrarPrepF.close();
+
+});
